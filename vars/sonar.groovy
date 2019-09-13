cc31b74dfbfb84f0d20756613457aa1542aa2fb2
mishraanupama661 <32787676+mishraanupama661@users.noreply.github.com>
2019-09-13 15:35:50 +0530

    updated

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -2,6 +2,7 @@ def call(){
             
          withSonarQubeEnv(credentialsId: 'sonar_anu', installationName: 'Sonar') {
     // some block
+                     waitForQualityGate abortPipeline: false, credentialsId: 'sonar_anu'
 }
 }
 
