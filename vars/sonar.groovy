6947e482055d889e4207f310edf4bc0ea565c22e
mishraanupama661 <32787676+mishraanupama661@users.noreply.github.com>
2019-09-17 15:57:48 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,12 +1,6 @@
 def call(){
             
- environment {
-           scannerHome=tool 'sonar scanner'
-       }
-            steps {
-                
-                sh "mvn sonar:sonar -Dsonar.host.url=http://18.224.155.110:9000"
-            }
+ 
 }
 
     
