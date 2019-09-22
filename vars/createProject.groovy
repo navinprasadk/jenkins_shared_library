6a695edf403facafa53fcc25d847e47e425a36b1
mishraanupama661 <32787676+mishraanupama661@users.noreply.github.com>
2019-09-22 22:45:03 +0530

    Update createProject.groovy

--- vars/createProject.groovy
+++ vars/createProject.groovy
@@ -1,6 +1,6 @@
 def call(){
 httpRequest authentication: 'bitbucket-new', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: '''{
-    "key": "TEST",
-    "name": "TEST_RIG"
+    "key": "TEST_12",
+    "name": "TEST_123"
 }''', responseHandle: 'NONE', url: 'http://18.224.68.30:7990/rest/api/1.0/projects'
 }
