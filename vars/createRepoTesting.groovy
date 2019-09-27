016eec67c17eb157c32d7936fe0a3203ad1cd76f
mishraanupama661 <32787676+mishraanupama661@users.noreply.github.com>
2019-09-27 15:49:11 +0530

    Update createRepoTesting.groovy

--- vars/createRepoTesting.groovy
+++ vars/createRepoTesting.groovy
@@ -1,8 +1,8 @@
 def call(){
 httpRequest authentication: 'bitbucket_anu', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: '''
 {
-"name": "tomcat_web",
+"name": "maven-web-application",
 "scmId": "git",
 "forkable": true
-}''', responseHandle: 'NONE', url: 'http://18.224.68.30:7990/rest/api/1.0/projects/KUS/repos'
+}''', responseHandle: 'NONE', url: 'http://18.224.68.30:7990/rest/api/1.0/projects/SHR/repos'
 }
