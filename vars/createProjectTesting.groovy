dc02e7197f520bb0d1caeb4d0cf8dd3a66afd662
mishraanupama661 <32787676+mishraanupama661@users.noreply.github.com>
2019-09-27 13:30:09 +0530

    Update createProjectTesting.groovy

--- vars/createProjectTesting.groovy
+++ vars/createProjectTesting.groovy
@@ -33,5 +33,15 @@ def projLength = resultJson.name.size()
  createProject(request)
 }
 
-  createRepoTesting()
+ def createRepoTesting()
+{
+  def call(){
+httpRequest authentication: 'bitbucket_anu', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: '''
+{
+"name": "tomcat_web_app",
+"scmId": "git",
+"forkable": true
+}''', responseHandle: 'NONE', url: 'http://18.224.68.30:7990/rest/api/1.0/projects/ANK/repos'
+}
+}
 
