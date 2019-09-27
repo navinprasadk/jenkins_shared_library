be176cf28c3f66a7c312e998af46b4680f6ccc0c
mishraanupama661 <32787676+mishraanupama661@users.noreply.github.com>
2019-09-27 13:57:31 +0530

    Create createQABranchTesting.groovy

---
+++ vars/createQABranchTesting.groovy
@@ -0,0 +1,7 @@
+def call(){
+httpRequest authentication: 'bitbucket_anu', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: '''{
+    "name": "QA",
+    "startPoint": "refs/heads/master"
+}''', responseHandle: 'NONE', url: 'http://18.224.68.30:7990/rest/api/1.0/projects/ANK/repos/tomcat_web_app/branches'
+
+}
