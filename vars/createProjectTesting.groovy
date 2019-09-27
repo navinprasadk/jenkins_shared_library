c0917391e758c86f9b375050affb874beb17a526
mishraanupama661 <32787676+mishraanupama661@users.noreply.github.com>
2019-09-27 13:01:10 +0530

    Update createProjectTesting.groovy

--- vars/createProjectTesting.groovy
+++ vars/createProjectTesting.groovy
@@ -32,3 +32,13 @@ def projLength = resultJson.name.size()
  def request = libraryResource 'data.json'
  createProject(request)
 }
+
+
+def call(){
+httpRequest authentication: 'bitbucket_anu', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: '''
+{
+"name": "tomcat_web_app",
+"scmId": "git",
+"forkable": true
+}''', responseHandle: 'NONE', url: 'http://18.224.68.30:7990/rest/api/1.0/projects/SRE/repos'
+}
