12880e2846d31271353c80e0ece75f747a72ba75
mishraanupama661 <32787676+mishraanupama661@users.noreply.github.com>
2019-09-27 13:46:38 +0530

    Update createProjectTesting.groovy

--- vars/createProjectTesting.groovy
+++ vars/createProjectTesting.groovy
@@ -33,15 +33,5 @@ def projLength = resultJson.name.size()
  createProject(request)
 }
 
-def call(){
-
-httpRequest authentication: 'bitbucket_anu', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: '''
-{
-"name": "web_app",
-"scmId": "git",
-"forkable": true
-}''', responseHandle: 'NONE', url: 'http://18.224.68.30:7990/rest/api/1.0/projects/ANK/repos'
-
-}
-
+createRepoTesting()
 
