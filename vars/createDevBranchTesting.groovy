aeb6c8ed7a4cdce439b48bde75f703e5239315ac
mishraanupama661 <32787676+mishraanupama661@users.noreply.github.com>
2019-09-30 12:55:32 +0530

    Update createDevBranchTesting.groovy

--- vars/createDevBranchTesting.groovy
+++ vars/createDevBranchTesting.groovy
@@ -2,6 +2,6 @@ def call(){
 httpRequest authentication: 'bitbucket_anu', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: '''{
     "name": "DEV",
     "startPoint": "refs/heads/master"
-}''', responseHandle: 'NONE', url: 'http://18.224.68.30:7990/rest/api/1.0/projects/DEM/repos/web-application_2/branches'
+}''', responseHandle: 'NONE', url: 'http://18.224.68.30:7990/rest/api/1.0/projects/DEM/repos/web_1/branches'
 
 }
