525eaa798830389d8c2b88b3086f13dcf8ba3185
mishraanupama661 <32787676+mishraanupama661@users.noreply.github.com>
2019-09-30 12:11:48 +0530

    Update createQABranchTesting.groovy

--- vars/createQABranchTesting.groovy
+++ vars/createQABranchTesting.groovy
@@ -2,6 +2,6 @@ def call(){
 httpRequest authentication: 'bitbucket_anu', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: '''{
     "name": "QA",
     "startPoint": "refs/heads/master"
-}''', responseHandle: 'NONE', url: 'http://18.224.68.30:7990/rest/api/1.0/projects/SHA/repos/web-application_1/branches'
+}''', responseHandle: 'NONE', url: 'http://18.224.68.30:7990/rest/api/1.0/projects/SHA/repos/web-application/branches'
 
 }
