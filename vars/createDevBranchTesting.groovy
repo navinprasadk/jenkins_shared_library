aaa136ac5c4de2a9a95081b5d0826533db83909f
mishraanupama661 <32787676+mishraanupama661@users.noreply.github.com>
2019-10-04 13:56:28 +0530

    Update createDevBranchTesting.groovy

--- vars/createDevBranchTesting.groovy
+++ vars/createDevBranchTesting.groovy
@@ -1,7 +1,31 @@
-def call(String text){
+import groovy.json.JsonSlurper 
+
+@NonCPS
+createDevBranch(String data){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+//def repoName = '"'+resultJson.repoName+'"'
+def projUrl = '"'+resultJson.url+'"'
+  
+  httpRequest authentication: 'bitbucket_anu', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: '''
+{
+    "name": "DEV",
+    "startPoint": "refs/heads/master"
+}''', responseHandle: 'NONE', url: resultJson.url+'DEM/repos/web_1/branches' 
+}
+
+
+def call(){
+ def request = libraryResource 'data.json'
+ createDevBranch(request)
+}
+
+
+
+/*def call(String text){
 httpRequest authentication: 'bitbucket_anu', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: '''{
     "name": "DEV",
     "startPoint": "refs/heads/master"
 }''', responseHandle: 'NONE', url: 'http://18.224.68.30:7990/rest/api/1.0/projects/DEM/repos/web_1/branches'
 
-}
+}*/
