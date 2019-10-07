0cc32368f8ffa7bcd06478efc3ff126e4b9ea893
mishraanupama661 <32787676+mishraanupama661@users.noreply.github.com>
2019-10-07 13:17:29 +0530

    Update createRepoTesting.groovy

--- vars/createRepoTesting.groovy
+++ vars/createRepoTesting.groovy
@@ -4,14 +4,14 @@ import groovy.json.JsonSlurper
 createRepo(String data){
 def jsonSlurper = new JsonSlurper() 
 def resultJson = jsonSlurper.parseText(data)
-String repoName2 = resultJson.repoName
+def repoName = '"'+resultJson.repoName+'"'
 def projUrl = resultJson.url
   
   println '"'+repoName2.toString()+'"'
   String repoName='"'+repoName2.toString()+'"'
   httpRequest authentication: 'bitbucket_anu', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: '''
 {
-    "name": '"'+repoName+'"',
+    "name": ${repoName},
     "scmId": "git",
     "forkable": true
 }''', responseHandle: 'NONE', url: projUrl +'DEM/repos' 
