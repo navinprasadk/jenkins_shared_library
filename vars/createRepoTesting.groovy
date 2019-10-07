18a9168b5f1dc74d02f1567756835e05740d2cb3
mishraanupama661 <32787676+mishraanupama661@users.noreply.github.com>
2019-10-07 12:58:36 +0530

    Update createRepoTesting.groovy

--- vars/createRepoTesting.groovy
+++ vars/createRepoTesting.groovy
@@ -11,7 +11,7 @@ def projUrl = resultJson.url
   
   httpRequest authentication: 'bitbucket_anu', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: '''
 {
-    "name": repoName.toString(),
+    "name": '"'+repoName.toString()+'"',
     "scmId": "git",
     "forkable": true
 }''', responseHandle: 'NONE', url: projUrl +'DEM/repos' 
