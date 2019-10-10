004cdee8184031f459e3c469b6f22c7302231023
mishraanupama661 <32787676+mishraanupama661@users.noreply.github.com>
2019-10-10 12:19:33 +0530

    Update createProjectTesting.groovy

--- vars/createProjectTesting.groovy
+++ vars/createProjectTesting.groovy
@@ -1,4 +1,20 @@
+import hudson.model.*
 import groovy.json.JsonSlurper 
+
+// get current thread / Executor
+def thr = Thread.currentThread()
+// get current build
+def build = thr?.executable
+ 
+ 
+// get parameters
+def parameters = build?.actions.find{ it instanceof ParametersAction }?.parameters
+parameters.each {
+   println "parameter ${it.name}:"
+   println it.dump()
+   println "-" * 80
+}
+
 
 @NonCPS
 createProject(String data){
