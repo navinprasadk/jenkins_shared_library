1c78af816393e3839331b49045527d4c04a5000a
mishraanupama661 <32787676+mishraanupama661@users.noreply.github.com>
2019-10-16 16:34:53 +0530

    Update createQABranchTesting.groovy

--- vars/createQABranchTesting.groovy
+++ vars/createQABranchTesting.groovy
@@ -1,7 +1,22 @@
-def call(){
-httpRequest authentication: 'bitbucket_anu', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: '''{
+import groovy.json.JsonSlurper 
+
+@NonCPS
+createDevBranch(String data, String URL){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+//def repoName = '"'+resultJson.repoName+'"'
+def projUrl = '"'+resultJson.url+'"'
+  
+  httpRequest authentication: 'bitbucket_anu', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: '''
+{
     "name": "QA",
     "startPoint": "refs/heads/master"
-}''', responseHandle: 'NONE', url: 'http://18.224.68.30:7990/rest/api/1.0/projects/DEM/repos/web_1/branches'
-
+}''', responseHandle: 'NONE', url: "http://" + "${URL}" + "/rest/api/1.0/projects/DEM/repos/web_1/branches"
 }
+
+
+def call(String BITBUCKETURL){
+ def request = libraryResource 'data.json'
+ createDevBranch(request,BITBUCKETURL)
+}
+
