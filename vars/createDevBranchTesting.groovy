c93c3832d38b11fcc9c893963963c46949634211
mishraanupama661 <32787676+mishraanupama661@users.noreply.github.com>
2019-10-17 11:57:29 +0530

    Update createDevBranchTesting.groovy

--- vars/createDevBranchTesting.groovy
+++ vars/createDevBranchTesting.groovy
@@ -4,14 +4,14 @@ import groovy.json.JsonSlurper
 createDevBranch(String data, String URL){
 def jsonSlurper = new JsonSlurper() 
 def resultJson = jsonSlurper.parseText(data)
-//def repoName = '"'+resultJson.repoName+'"'
+def branchName = '"'+resultJson.branch1+'"'
 def projUrl = '"'+resultJson.url+'"'
   
-  httpRequest authentication: 'bitbucket_anu', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: '''
+  httpRequest authentication: 'bitbucket_anu', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
 {
-    "name": "DEV",
+    "name": ${branchName},
     "startPoint": "refs/heads/master"
-}''', responseHandle: 'NONE', url: "http://" + "${URL}" + "/rest/api/1.0/projects/DEM/repos/web_1/branches"
+}""", responseHandle: 'NONE', url: "http://" + "${URL}" + "/rest/api/1.0/projects/DEM/repos/web_1/branches"
 }
 
 
